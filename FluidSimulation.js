d1603bbf38ca0aa0f684588bac5be3cfff1c2988
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-08-02 19:43:50 +0200

    bruh  the CSS duh

--- FluidSimulation.js
+++ FluidSimulation.js
@@ -95,7 +95,7 @@ fn vertexMain(@location(0) pos: vec2<f32>) -> VertexOutput {
 
 @fragment
 fn fragmentMain() -> @location(0) vec4<f32> {
-    return vec4<f32>(1.0, 0.0, 0.0, 0.35);
+    return vec4<f32>(1.0, 0.0, 0.0, 1.0);
 }
 `;
 
